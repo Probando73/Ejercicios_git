520eb360e734cb5e7fb04b01cf899ccead1ae610
Probando73 <153937042+Probando73@users.noreply.github.com>
2024-04-19 12:04:57 -0300

    Delete Level_0/ejercicio_1.py

--- Level_0/ejercicio_1.py
+++
@@ -1,7 +0,0 @@
-"""1. Cree un sistema de banca en línea con las siguientes características:
-
-* Los usuarios deben poder iniciar sesión con un nombre de usuario y contraseña.
-* Si el usuario ingresa las credenciales incorrectas tres veces, el sistema debe bloquearlas.
-*El saldo inicial en la cuenta bancaria es de $2000.
-* El sistema debe permitir a los usuarios depositar, retirar, ver y transferir dinero.
-* El sistema debe mostrar un menú para que los usuarios realicen transacciones."""
